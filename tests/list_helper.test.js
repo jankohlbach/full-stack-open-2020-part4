bc75349059153bc75163f81b2f364cd66b5897cc
Jan Kohlbach <mail@jankohlbach.dev>
2020-08-31 17:09:16 +0200

    exercise 4.5

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -53,3 +53,102 @@ describe('total likes', () => {
     expect(result).toBe(202);
   });
 });
+
+describe('favorite blog', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ];
+
+  const listWithMoreBlogs = [
+    {
+      _id: '5f4cc996c00c4d2108c780da',
+      title: 'Titel 1',
+      author: 'Me',
+      url: 'https://jankohlbach.com',
+      likes: 101,
+      __v: 0
+    },
+    {
+      _id: '5f4cf80dc100a40f80d494fb',
+      title: 'Titel 3',
+      author: 'Me',
+      url: 'https://jankohlbach.com',
+      likes: 120,
+      __v: 0
+    },
+    {
+      _id: '5f4cf80dc100a30f80d494fb',
+      title: 'Titel 2',
+      author: 'Me',
+      url: 'https://jankohlbach.com',
+      likes: 101,
+      __v: 0
+    }
+  ];
+
+  const listWithMoreBlogsSameLikes = [
+    {
+      _id: '5f4cc996c00c4d2108c780da',
+      title: 'Titel 1',
+      author: 'Me',
+      url: 'https://jankohlbach.com',
+      likes: 101,
+      __v: 0
+    },
+    {
+      _id: '5f4cf80dc100a40f80d494fb',
+      title: 'Titel 3',
+      author: 'Me',
+      url: 'https://jankohlbach.com',
+      likes: 120,
+      __v: 0
+    },
+    {
+      _id: '5f4cf80dc100a30f80d494fb',
+      title: 'Titel 2',
+      author: 'Me',
+      url: 'https://jankohlbach.com',
+      likes: 120,
+      __v: 0
+    }
+  ];
+
+  test('when list has only one blog, entry is the most favorite', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog);
+    expect(result).toEqual({
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      likes: 5,
+    });
+  });
+
+  test('when list has no blogs, return null', () => {
+    const result = listHelper.favoriteBlog([]);
+    expect(result).toBe(null);
+  });
+
+  test('when list has more blogs, return most favorite', () => {
+    const result = listHelper.favoriteBlog(listWithMoreBlogs);
+    expect(result).toEqual({
+      title: 'Titel 3',
+      author: 'Me',
+      likes: 120,
+    });
+  });
+
+  test('when list has more blogs with same like count, return last one', () => {
+    const result = listHelper.favoriteBlog(listWithMoreBlogsSameLikes);
+    expect(result).toEqual({
+      title: 'Titel 2',
+      author: 'Me',
+      likes: 120,
+    });
+  });
+});
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,7 +2,25 @@ const dummy = (blogs) => 1;
 
 const totalLikes = (blogs) => blogs.reduce((acc, curr) => acc + curr.likes, 0);
 
+const favoriteBlog = (blogs) => {
+  if(blogs.length === 0) return null;
+
+  return blogs.reduce((prev, curr) => prev.likes > curr.likes
+    ? {
+      title: prev.title,
+      author: prev.author,
+      likes: prev.likes,
+    }
+    : {
+      title: curr.title,
+      author: curr.author,
+      likes: curr.likes,
+    }
+  , {});
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
