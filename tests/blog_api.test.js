c5dc23b168469434f7af312ffcebf48f775f68a7
Jan Kohlbach <mail@jankohlbach.dev>
2020-09-01 22:38:04 +0200

    exercise 4.13

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -92,6 +92,21 @@ test('missing title and url properties should resond with 400 Bad Request', asyn
     .expect(400);
 });
 
+test('status code 204 when deleting with valid id', async () => {
+  const blogsAtStart = await helper.blogsInDb();
+  const blogToDelete = blogsAtStart[0];
+
+  await api
+    .delete(`/api/blogs/${blogToDelete.id}`)
+    .expect(204);
+
+  const blogsAtEnd = await helper.blogsInDb();
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1);
+
+  const contents = blogsAtEnd.map(blog => blog.title);
+  expect(contents).not.toContain(blogToDelete.title);
+});
+
 afterAll(() => {
   mongoose.connection.close();
 });
--- tests/test_helper.js
+++ tests/test_helper.js
@@ -17,7 +17,7 @@ const initialBlogs = [
 
 const blogsInDb = async () => {
   const blogs = await Blog.find({});
-  return blogs;
+  return blogs.map(blog => blog.toJSON());
 };
 
 module.exports = {
