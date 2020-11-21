5bcd41846fdd7fa914435a9444f39d4752f8271a
Jan Kohlbach <mail@jankohlbach.dev>
2020-11-21 13:42:03 +0100

    exercise 4.17

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,19 +1,26 @@
 const blogRouter = require('express').Router();
 const Blog = require('./../models/blog');
+const User = require('./../models/user');
 
 blogRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate('user', {username: 1, name: 1, id: 1});
   response.json(blogs);
 });
 
 blogRouter.post('/', async (request, response) => {
+  const users = await User.find({});
+  const user = users[0];
+
   const blog = new Blog({
     title: request.body.title,
     author: request.body.author,
     url: request.body.url,
     likes: request.body.likes || 0,
+    user: user._id,
   });
   const result = await blog.save();
+  user.blogs = user.blogs.concat(result._id);
+  await user.save();
   response.status(201).json(result);
 });
 
