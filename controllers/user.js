41068e791176aece7f85543645189bea2cfad01d
Jan Kohlbach <mail@jankohlbach.dev>
2020-11-21 13:13:04 +0100

    exercise 4.16

--- controllers/user.js
+++ controllers/user.js
@@ -3,6 +3,14 @@ const userRouter = require('express').Router();
 const User = require('./../models/user');
 
 userRouter.post('/', async (request, response) => {
+  if(!request.body.password) {
+    response.status(400).json({error: 'password field missing'});
+  }
+
+  if(request.body.password.length < 3) {
+    response.status(400).json({error: 'password must be at least 3 characters'});
+  }
+
   const saltRounds = 10;
   const passwordHash = await bcrypt.hash(request.body.password, saltRounds);
 
---
+++ tests/user_api.test.js
@@ -0,0 +1,86 @@
+const supertest = require('supertest');
+const bcrypt = require('bcrypt');
+const helper = require('./test_helper');
+const app = require('./../app');
+const User = require('../models/user');
+
+const api = supertest(app);
+
+beforeEach(async () => {
+  await User.deleteMany({});
+
+  const passwordHash = await bcrypt.hash('admin', 10);
+  const user = new User({
+    username: 'root',
+    passwordHash,
+  });
+
+  await user.save();
+});
+
+test('invalid user is not created', async () => {
+  const usersAtStart = await helper.usersInDb();
+
+  const newUserNoUsername = {
+    password: 'admin',
+  };
+
+  const newUserNoPassword = {
+    username: 'jan',
+  };
+
+  const newUserShortUsername = {
+    username: 'ja',
+    password: 'admin',
+  };
+
+  const newUserShortPassword = {
+    username: 'jan',
+    password: 'ad',
+  };
+
+  await api
+    .post('/api/users')
+    .send(newUserNoUsername)
+    .expect(400);
+
+  await api
+    .post('/api/users')
+    .send(newUserNoPassword)
+    .expect(400);
+
+  await api
+    .post('/api/users')
+    .send(newUserShortUsername)
+    .expect(400);
+
+  await api
+    .post('/api/users')
+    .send(newUserShortPassword)
+    .expect(400);
+
+  const usersAtEnd = await helper.usersInDb();
+  expect(usersAtEnd).toHaveLength(usersAtStart.length);
+});
+
+test('new user can be added', async () => {
+  const usersAtStart = await helper.usersInDb();
+
+  const newUser = {
+    username: 'testuser',
+    name: 'Test User',
+    password: 'admin',
+  };
+
+  await api
+    .post('/api/users')
+    .send(newUser)
+    .expect(200)
+    .expect('content-type', /application\/json/);
+
+  const usersAtEnd = await helper.usersInDb();
+  expect(usersAtEnd).toHaveLength(usersAtStart.length + 1);
+
+  const users = usersAtEnd.map(user => user.username);
+  expect(users).toContain('jan');
+});
